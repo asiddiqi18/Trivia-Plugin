c1f01d3d6f57de78dd04f1677c53880c8398f470
MarCarrot <60483997+MarCarrot@users.noreply.github.com>
2021-04-17 14:56:02 -0500

    Switched from list to hashmap

--- src/main/java/me/marcarrots/trivia/PlayerScoreHolder.java
+++ src/main/java/me/marcarrots/trivia/PlayerScoreHolder.java
@@ -5,58 +5,50 @@ import me.marcarrots.trivia.Language.LangBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class PlayerScoreHolder {
 
-    private final List<PlayerScore> scores;
+    private final HashMap<String, PlayerScore> scores;
     private final Trivia trivia;
 
     public PlayerScoreHolder(Trivia trivia) {
-        scores = new ArrayList<>();
+        scores = new HashMap<>();
         this.trivia = trivia;
     }
 
-    public void addPlayersToGame() {
+    public void addOnlinePlayersToGame() {
         scores.clear();
         Collection<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers();
         for (Player player : onlinePlayers) {
-            scores.add(new PlayerScore(player));
+            scores.put(player.getName(), new PlayerScore((player)));
         }
-
     }
 
-    public void updatePlayersToGame(Player player) {
-        for (PlayerScore score : scores) {
-            if (score.getPlayer().getName().equals(player.getName())) {
-                score.setPlayer(player);
-                return;
-            }
+    public void addPlayerToGame(Player player) {
+        if (!scores.containsKey(player.getName())) {
+            scores.put(player.getName(), new PlayerScore(player));
         }
-        scores.add(new PlayerScore(player));
     }
 
     public void addScore(Player player) {
-        for (PlayerScore score : scores) {
-            if (score.getPlayer().getName().equals(player.getName())) {
-                score.addScore();
-            }
-
-        }
+        PlayerScore score = scores.get(player.getName());
+        score.incrementScore();
+        scores.put(player.getName(), score);
     }
 
     public void broadcastLargestScores() {
         int displayAmount = Math.min(scores.size(), trivia.getConfig().getInt("Top winner amount", 3));
-        Collections.sort(scores);
-        if (scores.size() == 0 || scores.get(0).getPoints() == 0) {
+
+        List<PlayerScore> scoreValues = new ArrayList<>(scores.values());
+        Collections.sort(scoreValues);
+
+        if (scoreValues.size() == 0 || scoreValues.get(0).getPoints() == 0) {
             Bukkit.broadcastMessage(Lang.TRIVIA_NO_WINNERS.format(null));
             return;
         }
         for (int i = 0; i < displayAmount; i++) {
-            final PlayerScore score = scores.get(i);
+            final PlayerScore score = scoreValues.get(i);
             if (score != null) {
                 if (score.getPoints() == 0) { // don't show winners if they didn't score at all.
                     break;
