72ccc116c507812a348a09b08c8879260e667077
MarCarrot <60483997+MarCarrot@users.noreply.github.com>
2021-05-31 17:25:13 -0500

    added executable commands after win event

--- src/main/java/me/marcarrots/trivia/Rewards.java
+++ src/main/java/me/marcarrots/trivia/Rewards.java
@@ -4,7 +4,10 @@
 
 package me.marcarrots.trivia;
 
+import me.marcarrots.trivia.Language.Lang;
+import me.marcarrots.trivia.Language.LangBuilder;
 import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -25,6 +28,7 @@ public class Rewards {
     double money;
     String message;
     List<ItemStack> items;
+    List<String> commands;
 
     public Rewards(Trivia trivia, int place) {
         items = new ArrayList<>();
@@ -81,32 +85,43 @@ public class Rewards {
         experience = trivia.getRewardsFile().getData().getInt(place + ".Experience");
         message = trivia.getRewardsFile().getData().getString(place + ".Message");
         items = (List<ItemStack>) trivia.getRewardsFile().getData().get(place + ".Items");
+        commands = (List<String>) trivia.getRewardsFile().getData().get(place + ".Commands");
     }
 
     public void giveReward(Player player) {
         // send reward message to player if there is one
-        if (getMessage() != null) {
+        if (message != null) {
             BukkitScheduler scheduler = getServer().getScheduler();
             scheduler.scheduleSyncDelayedTask(trivia, () -> player.sendMessage(getMessage()), 3);
         }
         // send money to player if vault is enabled
-        if (trivia.vaultEnabled() && getMoney() > 0) {
+        if (trivia.vaultEnabled() && money > 0) {
             EconomyResponse r = Trivia.getEcon().depositPlayer(player, getMoney());
             if (!r.transactionSuccess()) {
                 player.sendMessage(String.format("An error occurred: %s", r.errorMessage));
             }
         }
+
+        if (commands != null) {
+            for (String command : commands) {
+                if (command.length() > 0) {
+                    command = Lang.fillPlaceholders(new LangBuilder().setPlayer(player), command);
+                    Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), command);
+                }
+            }
+        }
+
         // send experience to player
-        if (getExperience() != 0) {
+        if (experience != 0) {
             player.giveExp(getExperience());
         }
         // if no item rewards are included, exit
-        if (getItems() == null || getItems().size() == 0) {
+        if (items == null || items.size() == 0) {
             return;
         }
         // otherwise, iterate through all items and give to player
         // drop item to ground if inventory full
-        for (ItemStack item : getItems()) {
+        for (ItemStack item : items) {
             if (player.getInventory().firstEmpty() == -1) {
                 player.getWorld().dropItem(player.getLocation(), item);
             } else {
